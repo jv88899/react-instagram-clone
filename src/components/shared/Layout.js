1791803fdb29c1f775b94dd0801ef6b9fbe682fb
jv88899 <jv88899@gmail.com>
2021-02-05 12:44:32 -0600

    set up basic SEO

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -2,12 +2,12 @@ import React from "react";
 import { useLayoutStyles } from "../../styles";
 import SEO from "../shared/Seo";
 
-function Layout({ children }) {
+function Layout({ children, title }) {
   const classes = useLayoutStyles();
 
   return (
     <section className={classes.section}>
-      <SEO />
+      <SEO title={title} />
       <main className={classes.main}>
         <section className={classes.childrenWrapper}>
           <div className={classes.children}>{children}</div>
