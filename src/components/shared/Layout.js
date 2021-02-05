099831ecfd09c3c9dfb423f09dc8fe867cd5e22c
jv88899 <jv88899@gmail.com>
2021-02-05 12:41:04 -0600

    imported SEO into layout

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useLayoutStyles } from "../../styles";
+import SEO from "../shared/Seo";
 
 function Layout({ children }) {
   const classes = useLayoutStyles();
 
   return (
     <section className={classes.section}>
+      <SEO />
       <main className={classes.main}>
         <section className={classes.childrenWrapper}>
           <div className={classes.children}>{children}</div>
