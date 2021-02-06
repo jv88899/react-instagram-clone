ac60861559f306a777a015238bb0cc8227afcd62
jv88899 <jv88899@gmail.com>
2021-02-06 10:57:34 -0600

    added feed post header to feedpost

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import { MoreIcon } from "../../icons";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
 
-  return <div>FeedPost</div>;
+  return (
+    <>
+      <article className={classes.article}>
+        {/* Feed Post Header */}
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.moreIcon} />
+        </div>
+      </article>
+    </>
+  );
 }
 
 export default FeedPost;
