43e20dc1998b08259325d6ba05863f90d5610c8a
jv88899 <jv88899@gmail.com>
2021-02-06 10:22:02 -0600

    created basic sign up form based off of login

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,81 @@
 import React from "react";
 import { useSignUpPageStyles } from "../styles";
+import SEO from "../components/shared/Seo";
+import {
+  Button,
+  Card,
+  CardHeader,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import LoginWithFacebook from "./login";
 
 function SignUpPage() {
-  useSignUpPageStyles();
+  const classes = useSignUpPageStyles();
 
-  return <div>SignUpPage</div>;
+  return (
+    <>
+      <SEO title="Sign up" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <div className={classes.cardHeader} />
+            <form>
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="username"
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Password"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="current-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={classes.button}
+                type="submit"
+              >
+                Log In
+              </Button>
+            </form>
+            <div className={classes.orContainer}>
+              <div className={classes.orLine}></div>
+              <div>
+                <Typography variant="body2" color="textSecondary">
+                  OR
+                </Typography>
+              </div>
+              <div className={classes.orLine}></div>
+            </div>
+            <LoginWithFacebook color="secondary" iconColor="blue" />
+            <Button fullWidth color="secondary">
+              <Typography variant="caption">Forgot password?</Typography>
+            </Button>
+          </Card>
+          <Card className={classes.signUpCard}>
+            <Typography align="right" variant="body2">
+              Don't have an account?
+            </Typography>
+            <Link to="/accounts/emailsignup">
+              <Button color="primary" className={classes.signUpButton}>
+                Sign up
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default SignUpPage;
