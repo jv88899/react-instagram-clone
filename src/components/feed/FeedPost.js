5a2303cc8c6da07a6bd28795a861a4a28680c419
jv88899 <jv88899@gmail.com>
2021-02-07 08:28:10 -0600

    created comment component

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -11,7 +11,13 @@ import {
   UnlikeIcon,
 } from "../../icons";
 import { Link } from "react-router-dom";
-import { Button, Divider, Hidden, Typography } from "@material-ui/core";
+import {
+  Button,
+  Divider,
+  Hidden,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
 function FeedPost({ post }) {
@@ -156,7 +162,35 @@ function SaveButton() {
 }
 
 function Comment() {
-  return <>Comment</>;
+  const classes = useFeedPostStyles();
+  const [content, setContent] = React.useState("");
+
+  return (
+    <div className={classes.commentContainer}>
+      <TextField
+        fullWidth
+        value={content}
+        placeholder="Add a comment..."
+        multilinerowsMax={2}
+        rows={1}
+        onChange={(event) => setContent(event.target.value)}
+        className={classes.textField}
+        InputProps={{
+          classes: {
+            root: classes.root,
+            underline: classes.underline,
+          },
+        }}
+      />
+      <Button
+        color="primary"
+        className={classes.commentButton}
+        disabled={!content.trim()}
+      >
+        Post
+      </Button>
+    </div>
+  );
 }
 
 export default FeedPost;
