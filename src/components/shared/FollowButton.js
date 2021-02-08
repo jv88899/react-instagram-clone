ee5a6bc71c5025a113f3a1a1ca8209518853725b
jv88899 <jv88899@gmail.com>
2021-02-08 16:13:50 -0600

    created follow button component

--- src/components/shared/FollowButton.js
+++ src/components/shared/FollowButton.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 import { useFollowButtonStyles } from "../../styles";
 
-function FollowButton() {
-  useFollowButtonStyles();
+function FollowButton({ side }) {
+  const classes = useFollowButtonStyles({ side });
+  const [isFollowing, setFollowing] = React.useState(false);
 
-  return <div>FollowButton</div>;
+  const followButton = (
+    <Button
+      variant={side ? "text" : "contained"}
+      color="primary"
+      className={classes.button}
+      onClick={() => setFollowing(true)}
+      fullWidth
+    >
+      Foloow
+    </Button>
+  );
+
+  const followingButton = (
+    <Button
+      variant={side ? "outlined" : "text"}
+      className={classes.button}
+      onClick={() => setFollowing(false)}
+      fullWidth
+    >
+      followButton
+    </Button>
+  );
+
+  return isFollowing ? followingButton : followButton;
 }
 
 export default FollowButton;
