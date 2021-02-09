d78663f56717c48f3f50ab688687e2adabf6768d
jv88899 <jv88899@gmail.com>
2021-02-09 15:37:11 -0600

    created followsuggestions component structure

--- src/components/shared/FollowSuggestions.js
+++ src/components/shared/FollowSuggestions.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import { LoadingLargeIcon } from "../../icons";
+import { getDefaultUser } from "../../data";
 import { useFollowSuggestionsStyles } from "../../styles";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function FollowSuggestions() {
-  useFollowSuggestionsStyles();
+  const classes = useFollowSuggestionsStyles();
 
-  return <div>FollowSuggestions</div>;
+  let loading = true;
+
+  return (
+    <div className={classes.container}>
+      <Typography
+        color="textSecondary"
+        variant="subtitle2"
+        className={classes.typography}
+      >
+        Suggestions for You
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <Slider
+          className={classes.slide}
+          dots={false}
+          infinite
+          speed={1000}
+          touchThreshold={1000}
+          variableWidth
+          swipeToSlide
+          arrows
+          slidesToScroll={3}
+          easing="ease-in-out"
+        >
+          {Array.from({ length: 10 }, () => getDefaultUser()).map((user) => (
+            <FollowSuggestionsItem key={user.id} user={user} />
+          ))}
+        </Slider>
+      )}
+    </div>
+  );
+}
+
+function FollowSuggestionsItem() {
+  return null;
 }
 
 export default FollowSuggestions;
