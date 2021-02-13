8af3276214fd01ba39155bcdbdc166a4d476ff5c
jv88899 <jv88899@gmail.com>
2021-02-13 12:00:19 -0600

    set up postmodal

--- src/components/post/PostModal.js
+++ src/components/post/PostModal.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Modal from "react-modal";
+import { useHistory } from "react-router-dom";
 import { usePostModalStyles } from "../../styles";
+import Post from "./Post";
 
 function PostModal() {
-  usePostModalStyles();
+  const history = useHistory();
+  const classes = usePostModalStyles();
 
-  return <div>PostModal</div>;
+  return (
+    <>
+      <Modal
+        isOpen
+        overlayClassName={classes.overlay}
+        onRequestClose={() => history.goBack()}
+        style={{
+          content: {
+            display: "flex",
+            alignItems: "center",
+            maxWidth: 935,
+            width: "100%",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+            margin: 0,
+            padding: 0,
+            overflow: "none",
+            WebkitOverflowScrolling: "touch",
+          },
+        }}
+      >
+        <Post />
+      </Modal>
+    </>
+  );
 }
 
 export default PostModal;
