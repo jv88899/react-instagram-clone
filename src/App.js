7790a36b976f2dac68376d3f3615b6346eb0ce52
jv88899 <jv88899@gmail.com>
2021-02-13 11:49:48 -0600

    added logic for showing modal

--- src/App.js
+++ src/App.js
@@ -13,18 +13,31 @@ import PostModal from "./components/post/PostModal";
 function App() {
   const history = useHistory();
   const location = useLocation();
+  const prevLocation = React.useRef(location);
+  const modal = location.state?.modal;
+
+  React.useEffect(() => {
+    if (history.action !== "POP" && !modal) {
+      prevLocation.current = location;
+    }
+  }, [location, modal, history.action]);
+
+  const isModalOpen = modal && prevLocation.current !== location;
 
   return (
-    <Switch>
-      <Route exact path="/" component={FeedPage} />
-      <Route path="/explore" component={ExplorePage} />
-      <Route exact path="/:username" component={ProfilePage} />
-      <Route exact path="/p/:postId" component={PostPage} />
-      <Route path="/accounts/edit" component={EditProfilePage} />
-      <Route path="/accounts/login" component={LoginPage} />
-      <Route path="/accounts/emailsignup" component={SignUpPage} />
-      <Route path="*" component={NotFoundPage} />
-    </Switch>
+    <>
+      <Switch location={isModalOpen ? prevLocation.current : location}>
+        <Route exact path="/" component={FeedPage} />
+        <Route path="/explore" component={ExplorePage} />
+        <Route exact path="/:username" component={ProfilePage} />
+        <Route exact path="/p/:postId" component={PostPage} />
+        <Route path="/accounts/edit" component={EditProfilePage} />
+        <Route path="/accounts/login" component={LoginPage} />
+        <Route path="/accounts/emailsignup" component={SignUpPage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+      {isModalOpen && <Route exact path="/p/:postId" component={PostModal} />}
+    </>
   );
 }
 
