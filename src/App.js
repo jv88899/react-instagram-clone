1b04a0efef28f65a0162581c06acd384de426797
jv88899 <jv88899@gmail.com>
2021-02-13 11:32:24 -0600

    moved browserrouter to index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import EditProfilePage from "./pages/edit-profile";
 import ExplorePage from "./pages/explore";
 import FeedPage from "./pages/feed";
@@ -8,21 +8,20 @@ import NotFoundPage from "./pages/not-found";
 import PostPage from "./pages/post";
 import ProfilePage from "./pages/profile";
 import SignUpPage from "./pages/signup";
+import PostModal from "./components/post/PostModal";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={FeedPage} />
-        <Route path="/explore" component={ExplorePage} />
-        <Route exact path="/:username" component={ProfilePage} />
-        <Route exact path="/p/:postId" component={PostPage} />
-        <Route path="/accounts/edit" component={EditProfilePage} />
-        <Route path="/accounts/login" component={LoginPage} />
-        <Route path="/accounts/emailsignup" component={SignUpPage} />
-        <Route path="*" component={NotFoundPage} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={FeedPage} />
+      <Route path="/explore" component={ExplorePage} />
+      <Route exact path="/:username" component={ProfilePage} />
+      <Route exact path="/p/:postId" component={PostPage} />
+      <Route path="/accounts/edit" component={EditProfilePage} />
+      <Route path="/accounts/login" component={LoginPage} />
+      <Route path="/accounts/emailsignup" component={SignUpPage} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
   );
 }
 
