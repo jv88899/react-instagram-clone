4d9bc350a0b5617eaeb23c474115115b58b0526d
jv88899 <jv88899@gmail.com>
2021-02-13 12:36:40 -0600

    copied exploregrid code to morepostsfromuser

--- src/components/post/MorePostsFromUser.js
+++ src/components/post/MorePostsFromUser.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { useMorePostsFromUserStyles } from "../../styles";
+import { Typography } from "@material-ui/core";
+import { useExploreGridStyles } from "../../styles";
+import { LoadingLargeIcon } from "../../icons";
+import { getDefaultPost } from "../../data";
+import GridPost from "../shared/GridPost";
 
-function MorePostsFromUser() {
-  useMorePostsFromUserStyles();
+function ExploreGrid() {
+  const classes = useExploreGridStyles();
 
-  return <div>MorePostsFromUser</div>;
+  let loading = false;
+
+  return (
+    <>
+      <Typography
+        color="textSecondary"
+        variant="subtitle2"
+        component="h2"
+        gutterBottom
+        className={classes.typography}
+      >
+        Explore
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <article className={classes.article}>
+          <div className={classes.postContainer}>
+            {Array.from({ length: 20 }, () => getDefaultPost()).map((post) => (
+              <GridPost key={post.id} post={post} />
+            ))}
+          </div>
+        </article>
+      )}
+    </>
+  );
 }
 
-export default MorePostsFromUser;
+export default ExploreGrid;
