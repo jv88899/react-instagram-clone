58276e5039316c2f35d9ff7079e0cc344656a8ad
jv88899 <jv88899@gmail.com>
2021-02-13 12:40:04 -0600

    made edits to morepostsfromuser

--- src/components/post/MorePostsFromUser.js
+++ src/components/post/MorePostsFromUser.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-import { useExploreGridStyles } from "../../styles";
+import { useMorePostsFromUserStyles } from "../../styles";
 import { LoadingLargeIcon } from "../../icons";
-import { getDefaultPost } from "../../data";
+import { getDefaultPost, defaultUser } from "../../data";
 import GridPost from "../shared/GridPost";
+import { Link } from "react-router-dom";
 
-function ExploreGrid() {
-  const classes = useExploreGridStyles();
+function MorePostsFromUser() {
+  const classes = useMorePostsFromUserStyles();
 
   let loading = false;
 
   return (
-    <>
+    <div className={classes.container}>
       <Typography
         color="textSecondary"
         variant="subtitle2"
@@ -19,21 +20,24 @@ function ExploreGrid() {
         gutterBottom
         className={classes.typography}
       >
-        Explore
+        More Posts from{" "}
+        <Link to={`/${defaultUser.username}`} className={classes.link}>
+          @{defaultUser.username}
+        </Link>
       </Typography>
       {loading ? (
         <LoadingLargeIcon />
       ) : (
         <article className={classes.article}>
           <div className={classes.postContainer}>
-            {Array.from({ length: 20 }, () => getDefaultPost()).map((post) => (
+            {Array.from({ length: 6 }, () => getDefaultPost()).map((post) => (
               <GridPost key={post.id} post={post} />
             ))}
           </div>
         </article>
       )}
-    </>
+    </div>
   );
 }
 
-export default ExploreGrid;
+export default MorePostsFromUser;
