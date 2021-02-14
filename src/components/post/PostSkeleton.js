92e08c0095aaf3dfcf8e9bc8bcc2b0ce2de67b85
jv88899 <jv88899@gmail.com>
2021-02-14 10:02:22 -0600

    reverted post skeleton component back to original

--- src/components/post/PostSkeleton.js
+++ src/components/post/PostSkeleton.js
@@ -2,20 +2,9 @@ import React from "react";
 import { usePostSkeletonStyles } from "../../styles";
 
 export function PostSkeleton() {
-  const classes = usePostSkeletonStyles();
+  usePostSkeletonStyles();
 
-  return (
-    <div className={classes.container}>
-      <div className={classes.headerSkeleton}>
-        <div className={classes.avatarSkeleton} />
-        <div className={classes.headerTextSkeleton}>
-          <div className={classes.primaryTextSkeleton} />
-          <div className={classes.secondaryTextSkeleton} />
-        </div>
-      </div>
-      <div className={classes.mediaSkeleton} />
-    </div>
-  );
+  return <div>PostSkeleton</div>;
 }
 
 export default PostSkeleton;
