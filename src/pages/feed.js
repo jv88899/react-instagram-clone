c5609da3f5df4610bcef968705094d928e95fbc5
jv88899 <jv88899@gmail.com>
2021-02-14 09:57:43 -0600

    add lazy loading of feedpost to feed page

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFeedPageStyles } from "../styles";
-import FeedPost from "../components/feed/FeedPost";
+// import FeedPost from "../components/feed/FeedPost";
 import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
 import Layout from "../components/shared/Layout";
 import UserCard from "../components/shared/UserCard";
@@ -8,6 +8,7 @@ import { getDefaultPost } from "../data";
 import { Hidden } from "@material-ui/core";
 import LoadingScreen from "../components/shared/LoadingScreen";
 import { LoadingLargeIcon } from "../icons";
+const FeedPost = React.lazy(() => import("../components/feed/FeedPost"));
 
 function FeedPage() {
   const classes = useFeedPageStyles();
@@ -23,7 +24,9 @@ function FeedPage() {
         <div>
           {Array.from({ length: 5 }, () => getDefaultPost()).map(
             (post, index) => (
-              <FeedPost key={post.id} index={index} post={post} />
+              <React.Suspense key={post.id} fallback={<>loading</>}>
+                <FeedPost index={index} post={post} />
+              </React.Suspense>
             )
           )}
         </div>
