095337cb6e82f2ad91a3a4cf469f0e1c7ccf849e
jv88899 <jv88899@gmail.com>
2021-02-14 10:00:02 -0600

    created postskeleton component

--- src/components/post/PostSkeleton.js
+++ src/components/post/PostSkeleton.js
@@ -2,9 +2,20 @@ import React from "react";
 import { usePostSkeletonStyles } from "../../styles";
 
 export function PostSkeleton() {
-  usePostSkeletonStyles();
+  const classes = usePostSkeletonStyles();
 
-  return <div>PostSkeleton</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.headerSkeleton}>
+        <div className={classes.avatarSkeleton} />
+        <div className={classes.headerTextSkeleton}>
+          <div className={classes.primaryTextSkeleton} />
+          <div className={classes.secondaryTextSkeleton} />
+        </div>
+      </div>
+      <div className={classes.mediaSkeleton} />
+    </div>
+  );
 }
 
 export default PostSkeleton;
