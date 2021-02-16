053324e88ea987ec7b00a86b68716933d04e4c64
jv88899 <jv88899@gmail.com>
2021-02-16 08:20:48 -0600

    added beginning structure to profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Layout from "../components/shared/Layout";
+import ProfilePicture from "../components/shared/ProfilePicture";
 import { useProfilePageStyles } from "../styles";
+import { defaultCurrentUser } from "../data";
+import { Card, CardContent, Hidden } from "@material-ui/core";
 
 function ProfilePage() {
-  useProfilePageStyles();
+  const classes = useProfilePageStyles();
 
-  return <Layout>ProfilePage</Layout>;
+  return (
+    <Layout
+      title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})`}
+    >
+      <div className={classes.container}>
+        <Hidden smUp>
+          <Card className={classes.cardLarge}>
+            <ProfilePicture />
+            <CardContent className={classes.cardContentLarge}></CardContent>
+          </Card>
+        </Hidden>
+        <Hidden xsDown></Hidden>
+      </div>
+    </Layout>
+  );
 }
 
 export default ProfilePage;
