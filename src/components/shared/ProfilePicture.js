44718df2bac658c02a901b7b16dd288cf28a05e1
jv88899 <jv88899@gmail.com>
2021-02-16 11:38:53 -0600

    added basic structure to profilepicture

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Person } from "@material-ui/icons";
 import { useProfilePictureStyles } from "../../styles";
 
-function ProfilePicture({ size, image, isOwner }) {
-  useProfilePictureStyles();
+function ProfilePicture({
+  size,
+  image = "https://picsum.photos/100",
+  isOwner,
+}) {
+  const classes = useProfilePictureStyles({ size, isOwner });
 
-  return <div>ProfilePicture</div>;
+  return (
+    <section className={classes.section}>
+      {image ? (
+        <div className={classes.wrapper}>
+          <img src={image} alt="user profile" className={classes.image} />
+        </div>
+      ) : (
+        <div className={classes.wrapper}>
+          <Person className={classes.person} />
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default ProfilePicture;
