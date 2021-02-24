718444820365f041193ac23d491cc0a8b52ab29e
jv88899 <jv88899@gmail.com>
2021-02-24 08:39:02 -0600

    created basic structure of editprofile page

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,10 +1,75 @@
 import React from "react";
+import { Drawer, Hidden, IconButton, List, ListItem } from "@material-ui/core";
+import Layout from "../components/shared/Layout";
 import { useEditProfilePageStyles } from "../styles";
+import { Menu } from "@material-ui/icons";
 
-function EditProfilePage() {
-  useEditProfilePageStyles();
+function EditProfilePage({ history }) {
+  const classes = useEditProfilePageStyles();
+  const [showDrawer, setDrawer] = React.useState(false);
 
-  return <div>EditProfilePage</div>;
+  function handleToggleDrawer() {
+    setDrawer((prev) => !prev);
+  }
+
+  function handleSelected(index) {
+    const path = history.location.pathname;
+    switch (index) {
+      case 0:
+        return path.includes("edit");
+      default:
+        break;
+    }
+  }
+
+  const options = [
+    "Edit Profile",
+    "Change Password",
+    "Apps and Websites",
+    "Email and SMS",
+    "Push Notifications",
+    "Manage Contacts",
+    "Privacy and Security",
+    "Login Activity",
+    "Emails from Instagram",
+  ];
+
+  const drawer = (
+    <List>
+      {options.map((option, index) => (
+        <ListItem
+          key={option}
+          button
+          selected={handleSelected(index)}
+        ></ListItem>
+      ))}
+    </List>
+  );
+
+  return (
+    <Layout title="Edit Profile">
+      <section className={classes.section}>
+        <IconButton
+          edge="start"
+          onClick={handleToggleDrawer}
+          className={classes.menuButton}
+        >
+          <Menu />
+        </IconButton>
+        <nav>
+          <Hidden smUp implementation="css">
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={showDrawer}
+              onClose={handleToggleDrawer}
+              classes={{ paperAnchorLeft: classes.temporaryDrawer }}
+            ></Drawer>
+          </Hidden>
+        </nav>
+      </section>
+    </Layout>
+  );
 }
 
 export default EditProfilePage;
