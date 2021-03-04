0da7819af65faff34b4b1263d965bc44ec5fc241
jv88899 <jv88899@gmail.com>
2021-03-03 19:35:08 -0600

    added ability to logout

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -16,8 +16,9 @@ import {
   Typography,
   Zoom,
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { GearIcon } from "../icons";
+import { AuthContext } from "../auth";
 
 function ProfilePage() {
   const classes = useProfilePageStyles();
@@ -244,10 +245,16 @@ function NameBioSection({ user }) {
 
 function OptionsMenu({ handleCloseMenu }) {
   const classes = useProfilePageStyles();
+  const { signOut } = React.useContext(AuthContext);
   const [showLogOutMessage, setLogOutMessage] = React.useState(false);
+  const history = useHistory();
 
   function handleLogOutClick() {
     setLogOutMessage(true);
+    setTimeout(() => {
+      signOut();
+      history.push("/accounts/login");
+    }, 2000);
   }
 
   return (
