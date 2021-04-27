247563b6a73c8d2b54e237b40e244b399d19a536
jv88899 <jv88899@gmail.com>
2021-04-27 18:46:46 -0500

    created loginwithemailandpassword function

--- src/auth.js
+++ src/auth.js
@@ -59,6 +59,13 @@ function AuthProvider({ children }) {
     await firebase.auth().signInWithPopup(provider);
   }
 
+  async function logInWithEmailAndPassword(email, password) {
+    const data = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+    return data;
+  }
+
   async function signUpWithEmailAndPassword(formData) {
     const data = await firebase
       .auth()
