e394d62e8341fb532ef82785ef75b2535cc21f67
jv88899 <jv88899@gmail.com>
2021-05-05 09:26:52 -0500

    added classes variable  to addpostdialog

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import { makeStyles } from "@material-ui/core";
+import { useAddPostDialogStyles } from "../../styles";
 
 const initialValue = [
   {
@@ -11,6 +12,7 @@ const initialValue = [
 ];
 
 function AddPostDialog({ media, handleClose }) {
+  const classes = useAddPostDialogStyles();
   const editor = React.useMemo(() => withReact(createEditor()), []);
   const [value, setValue] = React.useState(initialValue);
 
