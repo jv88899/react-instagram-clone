f2339b7bf34c9b67015c3503f59fbac4e49726d0
jv88899 <jv88899@gmail.com>
2021-05-05 10:58:08 -0500

    created structure of addpostdialog

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -1,7 +1,21 @@
 import React from "react";
+import {
+  AppBar,
+  Avatar,
+  Button,
+  Dialog,
+  Divider,
+  InputAdornment,
+  Paper,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import { useAddPostDialogStyles } from "../../styles";
+import { ArrowBackIos, PinDrop } from "@material-ui/icons";
+import { UserContext } from "../../App";
 
 const initialValue = [
   {
@@ -12,14 +26,62 @@ const initialValue = [
 
 function AddPostDialog({ media, handleClose }) {
   const classes = useAddPostDialogStyles();
+  const { me } = React.useContext(UserContext);
   const editor = React.useMemo(() => withReact(createEditor()), []);
   const [value, setValue] = React.useState(initialValue);
+  const [location, setLocation] = React.useState("");
 
-  //   <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
-  //     <Editable />
-  //   </Slate>;
-
-  return <span>addpostdialog</span>;
+  return (
+    <Dialog fullScreen open onClose={handleClose}>
+      <AppBar className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <ArrowBackIos onClick={handleClose} />
+          <Typography align="center" variant="body1" className={classes.title}>
+            New Post
+          </Typography>
+          <Button color="primary" className={classes.share}>
+            Share
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Divider />
+      <Paper className={classes.paper}>
+        <Avatar src={me.profile_image} />
+        <Slate
+          editor={editor}
+          value={value}
+          onChange={(value) => setValue(value)}
+        >
+          <Editable
+            className={classes.editor}
+            placeholder="Write your caption..."
+          />
+        </Slate>
+        <Avatar
+          src={URL.createObjectURL(media)}
+          className={classes.avatarLarge}
+          variant="square"
+        />
+      </Paper>
+      <TextField
+        fullWidth
+        placeholder="Location"
+        InputProps={{
+          classes: {
+            root: classes.root,
+            input: classes.input,
+            underline: classes.underline,
+          },
+          startAdornment: (
+            <InputAdornment>
+              <PinDrop />
+            </InputAdornment>
+          ),
+        }}
+        onChange={(event) => setLocation(event.target.value)}
+      />
+    </Dialog>
+  );
 }
 
 export default AddPostDialog;
