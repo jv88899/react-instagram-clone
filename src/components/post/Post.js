424a5294ddc72cd36d3c16a41f41b0c834eea3e9
jv88899 <jv88899@gmail.com>
2021-06-01 19:55:42 -0500

    commented out part of the variables object in Post

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -209,7 +209,7 @@ function LikeButton({ likes, authorId, postId }) {
   const variables = {
     postId,
     userId: currentUserId,
-    profileId: authorId,
+    // profileId: authorId,
   };
 
   function handleLike() {
