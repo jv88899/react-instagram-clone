cdca1b6aa9aad6a92e0ed826e36751fd90b6f20b
jv88899 <jv88899@gmail.com>
2021-06-10 19:03:04 -0500

    created formatDateToNowShort utility function

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,8 +1,15 @@
-import { format, isThisYear } from "date-fns";
+import { format, formatDistanceStrict, isThisYear } from "date-fns";
 
 export function formatPostDate(date) {
   const formatShort = format(new Date(date), "MMMM d");
   const formatLong = format(new Date(date), "MMMM d yyy");
 
   return isThisYear(new Date(date)) ? formatShort : formatLong;
+}
+
+export function formatDateToNowShort(date) {
+  return formatDistanceStrict(new Date(date), new Date(Date.now()))
+    .split(" ")
+    .map((s, i) => (i === 1 ? s[0] : s))
+    .join("");
 }
