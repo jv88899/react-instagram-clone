f060e8f89537303982209e7f799ee4f54196e608
jv88899 <jv88899@gmail.com>
2021-06-22 17:01:05 -0500

    added actual data to ExploreGrid component

--- src/components/explore/ExploreGrid.js
+++ src/components/explore/ExploreGrid.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 import { useExploreGridStyles } from "../../styles";
 import { LoadingLargeIcon } from "../../icons";
-import { getDefaultPost } from "../../data";
 import GridPost from "../shared/GridPost";
+import { useQuery } from "@apollo/react-hooks";
+import { EXPLORE_POSTS } from "../../graphql/queries";
+import { UserContext } from "../../App";
 
 function ExploreGrid() {
   const classes = useExploreGridStyles();
-
-  let loading = false;
+  const { followingIds } = React.useContext(UserContext);
+  const variables = { followingIds };
+  const { data, loading } = useQuery(EXPLORE_POSTS, { variables });
 
   return (
     <>
@@ -26,7 +29,7 @@ function ExploreGrid() {
       ) : (
         <article className={classes.article}>
           <div className={classes.postContainer}>
-            {Array.from({ length: 20 }, () => getDefaultPost()).map((post) => (
+            {data.posts.map((post) => (
               <GridPost key={post.id} post={post} />
             ))}
           </div>
