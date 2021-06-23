55ada99febc9ec474da7689901007fe732b62370
jv88899 <jv88899@gmail.com>
2021-06-22 19:54:21 -0500

    added functionality to OptionsDialog

--- src/components/shared/OptionsDialog.js
+++ src/components/shared/OptionsDialog.js
@@ -1,11 +1,43 @@
 import React from "react";
 import { Button, Dialog, Divider, Zoom } from "@material-ui/core";
 import { useOptionsDialogStyles } from "../../styles";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../../App";
+import { useMutation } from "@apollo/react-hooks";
+import { DELETE_POST, UNFOLLOW_USER } from "../../graphql/mutations";
 
 function OptionsDialog({ onClose, postId, authorId }) {
   const classes = useOptionsDialogStyles();
+  const { currentUserId, followingIds } = React.useContext(UserContext);
+  const isOwner = authorId === currentUserId;
+  const buttonText = isOwner ? "Delete" : "Unfollow";
+  const onClick = isOwner ? handleDeletePost : handleUnfollowUser;
+  const isFollowing = followingIds.some((id) => id === authorId);
+  const isUnrelatedUser = !isOwner && !isFollowing;
+  const [unfollowUser] = useMutation(UNFOLLOW_USER);
+  const [deletePost] = useMutation(DELETE_POST);
+
+  const history = useHistory();
+
+  async function handleDeletePost() {
+    const variables = {
+      postId,
+      userId: currentUserId,
+    };
+    await deletePost({ variables });
+    onClose();
+    history.push("/");
+    window.location.reload();
+  }
+
+  function handleUnfollowUser() {
+    const variables = {
+      userIdToFollow: authorId,
+      currentUserId,
+    };
+    unfollowUser({ variables });
+    onClose();
+  }
 
   return (
     <Dialog
@@ -16,7 +48,11 @@ function OptionsDialog({ onClose, postId, authorId }) {
       onClose={onClose}
       TransitionComponent={Zoom}
     >
-      <Button className={classes.redButton}>Unfollow</Button>
+      {!isUnrelatedUser && (
+        <Button onClick={onClick} className={classes.redButton}>
+          {buttonText}
+        </Button>
+      )}
       <Divider />
       <Button className={classes.button}>
         <Link to={`/p/${postId}`}>Go to post</Link>
