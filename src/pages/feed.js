70e34d5f1edeed0e9cfd945c8c70ae88ad091059
jv88899 <jv88899@gmail.com>
2021-06-22 20:55:32 -0500

    added functionality for working feed

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -8,13 +8,18 @@ import { getDefaultPost } from "../data";
 import { Hidden } from "@material-ui/core";
 import LoadingScreen from "../components/shared/LoadingScreen";
 import { LoadingLargeIcon } from "../icons";
+import { UserContext } from "../App";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_FEED } from "../graphql/queries";
 const FeedPost = React.lazy(() => import("../components/feed/FeedPost"));
 
 function FeedPage() {
   const classes = useFeedPageStyles();
+  const { me, feedIds } = React.useContext(UserContext);
   const [isEndOfFeed] = React.useState(false);
+  const variables = { feedIds, limit: 2 };
+  const { data, loading } = useQuery(GET_FEED, { variables });
 
-  let loading = false;
   if (loading) return <LoadingScreen />;
 
   return (
@@ -22,19 +27,17 @@ function FeedPage() {
       <div className={classes.container}>
         {/* Feed post */}
         <div>
-          {Array.from({ length: 5 }, () => getDefaultPost()).map(
-            (post, index) => (
-              <React.Suspense key={post.id} fallback={<FeedPostSkeleton />}>
-                <FeedPost index={index} post={post} />
-              </React.Suspense>
-            )
-          )}
+          {data.posts.map((post, index) => (
+            <React.Suspense key={post.id} fallback={<FeedPostSkeleton />}>
+              <FeedPost index={index} post={post} />
+            </React.Suspense>
+          ))}
         </div>
         {/* Sidebar */}
         <Hidden smDown>
           <div className={classes.sidebarContainer}>
             <div className={classes.sidebarWrapper}>
-              <UserCard avatarSize={50} />
+              <UserCard user={me} avatarSize={50} />
               <FeedSideSuggestions />
             </div>
           </div>
